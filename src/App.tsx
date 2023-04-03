ec4be377d909dddf675816c543129db8382919fd
mukhammad <mukhammaddev9@gmail.com>
2023-04-03 12:21:02 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="wrapper">
+            <div className="__container">
+                <code>src/App.tsx</code>
+            </div>
+        </div>
+    );
+}
+
+export default App;
