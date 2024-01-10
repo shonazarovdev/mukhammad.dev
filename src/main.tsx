5b3ae0a961a5a9122e13fbaba4ff4bd59b00b34e
Mukhammad <mukhammaddev9@gmail.com>
2024-01-10 13:34:59 +0500

    Removed Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./styles/main.scss";
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-// @ts-ignore
-injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
